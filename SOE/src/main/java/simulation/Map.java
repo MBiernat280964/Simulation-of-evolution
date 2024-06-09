0f9f8e11954ef7cc91f7bb85a486a4b212eb825d
Magda <280964@student.pwr.edu.pl>
2024-06-09 17:39:34 +0200

    Changes map configuration along with methods populating the map with animals

--- SOE/src/main/java/simulation/Map.java
+++ SOE/src/main/java/simulation/Map.java
@@ -10,29 +10,12 @@ import java.lang.Math;
  */
 
 public class Map {
-    final public int sizeOfMap = 20;
-    //final public int sizeOfMap = 50;
-    //boolean [][] biome = new boolean [sizeOfMap][sizeOfMap];
-    //Creature[][] mapOfCreatures = new Creature[sizeOfMap][sizeOfMap];
-
-    //map - char array [][][] layer 0: biome, layer 1: creatures
+    final public int sizeOfMap = 100;
 
     char [][][] map = new char [2][sizeOfMap][sizeOfMap];
 
-    /*public Map(int num) {
-        this.population = new ArrayList<>();
-        if (num == 1)
-            createMap1();
-        else if (num == 2)
-            createMap2();
-        else if (num == 3)
-            createMap3();
-    }*/
-
     public Map() {
-        this.genMapLake();
-        this.genMapIsland();
-
+        this.genMapRiver();
     }
 
     void showMap() {
@@ -50,15 +33,12 @@ public class Map {
         }
     }
 
-
-
-    //on layer 0: W means water while L means land
-    //on layer 1: creatures
-    void genMapLake() //lake map
-    {
+    void genMapLake() {
         for (int i = 0; i < sizeOfMap; i++) {
             for (int j = 0; j < sizeOfMap; j++) {
-                if (i >= (j-sizeOfMap/2)*(j-sizeOfMap/2)/20 + 15 && (-(j-sizeOfMap/2)*(j-sizeOfMap/2))/20 +30 > 0 && i <= (-(j-sizeOfMap/2)*(j-sizeOfMap/2))/4 +3*sizeOfMap/4){
+                if (i >= (j-sizeOfMap/2)*(j-sizeOfMap/2)/20 + 15 &&
+                        (-(j-sizeOfMap/2)*(j-sizeOfMap/2))/20 +30 > 0 &&
+                        i <= (-(j-sizeOfMap/2)*(j-sizeOfMap/2))/4 +3*sizeOfMap/4){
                     this.map[0][i][j] = 'W';
                 } else {
                     this.map[0][i][j] = 'L';
@@ -67,27 +47,27 @@ public class Map {
         }
     }
 
-    void genMapRiver() //river map
-    {
+    void genMapRiver() {
         for (int i = 0; i < sizeOfMap; i++) {
             for (int j = 0; j < sizeOfMap; j++) {
-                if (4*Math.sin(((float)j/3)) + 34 >= i){
+                if (4*Math.sin(((float)j/5)) + 34 >= i){
                     this.map[0][i][j] = 'W';
                 } else {
                     this.map[0][i][j] = 'L';
                 }
-                if (4*Math.sin(((float)j/3)) + 30 >= i){
+                if (4*Math.sin(((float)j/5)) + 20 >= i){
                     this.map[0][i][j] = 'L';
                 }
             }
         }
     }
 
-    void genMapIsland() //island map
-    {
+    void genMapIsland() {
         for (int i = 0; i < sizeOfMap; i++) {
             for (int j = 0; j < sizeOfMap; j++) {
-                if (i >= (j-sizeOfMap/2)*(j-sizeOfMap/2)/20 + 5 && (-(j-sizeOfMap/2)*(j-sizeOfMap/2))/20 +30 > 0 && i <= (-(j-sizeOfMap/2)*(j-sizeOfMap/2))/4 +sizeOfMap - 4){
+                if (i >= (j-sizeOfMap/2)*(j-sizeOfMap/2)/20 + 5 &&
+                        (-(j-sizeOfMap/2)*(j-sizeOfMap/2))/20 +30 > 0 &&
+                        i <= (-(j-sizeOfMap/2)*(j-sizeOfMap/2))/4 +sizeOfMap - 4){
                     this.map[0][i][j] = 'L';
                 } else {
                     this.map[0][i][j] = 'W';
