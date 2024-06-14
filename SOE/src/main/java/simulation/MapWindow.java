3a82c734a4a2881d8e2c5277d3048092cb14908d
MBiernat280964 <280964@student.pwr.edu.pl>
2024-06-14 14:43:00 +0200

    Implements partial map view updates in GUI

--- SOE/src/main/java/simulation/MapWindow.java
+++ SOE/src/main/java/simulation/MapWindow.java
@@ -4,32 +4,60 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MapWindow {
-    public static void main(String[] args) {
-        Map map = new Map("island");
+    Map map = new Map("island");
+    JPanel [][] jPanelArray = new JPanel[map.sizeOfMap][map.sizeOfMap];
 
+    public MapWindow() {
+        prepareGUI();
+    }
+
+    public static void main(String[] args) {
+        MapWindow mapop = new MapWindow();
+
+
+        /*EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new mapop.display();
+            }
+        }*/
+    }
+
+    private void prepareGUI() {
         JFrame frame = new JFrame("Map");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLayout(new GridLayout(map.sizeOfMap,map.sizeOfMap));
         frame.setLocationRelativeTo(null);
-
-        map.showMap();
-
         for (int i = 0; i < map.sizeOfMap ; i++) {
             for ( int j = 0; j < map.sizeOfMap ; j++) {
-                JPanel lol = new JPanel();
+                this.jPanelArray[i][j] = new JPanel();
                 if (map.map[0][i][j] == 'L') {
-                    lol.setBackground(Color.green);
+                    this.jPanelArray[i][j].setBackground(Color.green);
                 }
                 else {
-                    lol.setBackground(Color.blue);
+                    this.jPanelArray[i][j].setBackground(Color.blue);
                 }
-                frame.add(lol);
+                frame.add(this.jPanelArray[i][j]);
+            }
+        }
+        frame.setVisible(true);
+    }
+
+    private void display() {
+        for (int i = 0; i < map.sizeOfMap ; i++) {
+            for ( int j = 0; j < map.sizeOfMap ; j++) {
+                if (map.map[1][i][j] != '\0') {
+                    this.jPanelArray[i][j].setBackground(Color.white);
+                } else if (map.map[0][i][j] == 'L') {
+                    this.jPanelArray[i][j].setBackground(Color.green);
+                } else {
+                    this.jPanelArray[i][j].setBackground(Color.blue);
+                }
             }
         }
 
-        frame.setVisible(true);
-
-
     }
 }
+
