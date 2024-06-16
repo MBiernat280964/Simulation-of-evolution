062d3d3d7e76d3d392dd589b07ff4202d5f1a7dd
MBiernat280964 <280964@student.pwr.edu.pl>
2024-06-16 19:48:30 +0200

    Fixes NullPointer errors in MapWindow class

--- SOE/src/main/java/simulation/MapWindow.java
+++ SOE/src/main/java/simulation/MapWindow.java
@@ -1,39 +1,30 @@
 package simulation;
 
+
 import javax.swing.*;
 import java.awt.*;
 
 public class MapWindow {
-    Map map = new Map("island");
-    JPanel [][] jPanelArray = new JPanel[map.sizeOfMap][map.sizeOfMap];
-
-    public MapWindow() {
+    private char[][][] map;
+    JPanel [][] jPanelArray = new JPanel[Map.sizeOfMap][Map.sizeOfMap];
+    JFrame frame;
+    public MapWindow(char [][][] map, JFrame frame) {
+        this.map = map;
+        this.frame = frame;
         prepareGUI();
     }
 
-    public static void main(String[] args) {
-        MapWindow mapop = new MapWindow();
-
-
-        /*EventQueue.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                new mapop.display();
-            }
-        }*/
-    }
-
     private void prepareGUI() {
-        JFrame frame = new JFrame("Map");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
-        frame.setLayout(new GridLayout(map.sizeOfMap,map.sizeOfMap));
+        frame.setLayout(new GridLayout(Map.sizeOfMap,Map.sizeOfMap));
         frame.setLocationRelativeTo(null);
-        for (int i = 0; i < map.sizeOfMap ; i++) {
-            for ( int j = 0; j < map.sizeOfMap ; j++) {
+        for (int i = 0; i < Map.sizeOfMap ; i++) {
+            for ( int j = 0; j < Map.sizeOfMap ; j++) {
                 this.jPanelArray[i][j] = new JPanel();
-                if (map.map[0][i][j] == 'L') {
+                if (this.map[1][i][j] != '\0') {
+                    this.jPanelArray[i][j].setBackground(Color.white);
+                }else if (this.map[0][i][j] == 'L') {
                     this.jPanelArray[i][j].setBackground(Color.green);
                 }
                 else {
@@ -45,12 +36,12 @@ public class MapWindow {
         frame.setVisible(true);
     }
 
-    private void display() {
-        for (int i = 0; i < map.sizeOfMap ; i++) {
-            for ( int j = 0; j < map.sizeOfMap ; j++) {
-                if (map.map[1][i][j] != '\0') {
+    public void updateDisplay() {
+        for (int i = 0; i < Map.sizeOfMap ; i++) {
+            for ( int j = 0; j < Map.sizeOfMap ; j++) {
+                if (this.map[1][i][j] != '\0') {
                     this.jPanelArray[i][j].setBackground(Color.white);
-                } else if (map.map[0][i][j] == 'L') {
+                } else if (this.map[0][i][j] == 'L') {
                     this.jPanelArray[i][j].setBackground(Color.green);
                 } else {
                     this.jPanelArray[i][j].setBackground(Color.blue);
@@ -60,4 +51,3 @@ public class MapWindow {
 
     }
 }
-
